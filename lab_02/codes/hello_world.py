8c537d4ce944d9e39a0694f0cf5d9a1170c8fbd9
Ondra <ondra.nohava@outlook.cz>
2023-08-31 21:10:59 +0200

    added codes for first lecture

---
+++ lab_02/codes/hello_world.py
@@ -0,0 +1,28 @@
+
+# základní hello world
+# tím začneme
+def hello():
+    print("Hello world")
+
+
+
+# složitější fce 
+def age():
+    print('Jak se jmenuješ?')
+    name = input()
+    print('Kolik ti je let?')
+    age = int(input())
+    print('Je ti:', age)
+
+
+# čtení textu
+print('Napiš text:')
+text = input()
+print("Tvůj text:", text, end='\n\n')
+
+
+# čtení čísla
+print('Napiš číslo:')
+num = int(input())
+print("Tvoje číslo:", num)
+
